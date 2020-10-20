d44e1433c6a0ae9ab3cc90a6949433ac1e2a9d83
Savagebeanman <72805052+Savagebeanman@users.noreply.github.com>
2020-10-20 16:52:31 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-
+#light.set_pixel_color(0, light.rgb(255, 0, 0))
+#light.set_pixel_color(1, light.rgb(0, 0, 255))
+#light.set_pixel_color(2, light.rgb(0, 255, 0))
+#light.set_pixel_color(3, light.rgb(255, 255, 0))
+#light.set_pixel_color(4, light.rgb(255, 0, 255))
+#light.set_pixel_color(5, light.rgb(0, 255, 255))
+#^^^^^^^^^ Code for first part
+light.set_pixel_color(0, light.rgb(255, 100, 0))
+light.set_pixel_color(1, light.rgb(255, 255, 0))
+light.set_pixel_color(2, light.rgb(0, 255, 0))
+light.set_pixel_color(3, light.rgb(0, 255, 100))
+light.set_pixel_color(4, light.rgb(0, 100, 125))
+light.set_pixel_color(5, light.rgb(0, 75, 255))
+light.set_pixel_color(6, light.rgb(0, 0, 255))
+light.set_pixel_color(7, light.rgb(100, 100, 200))
+light.set_pixel_color(8, light.rgb(255, 0, 105))
+light.set_pixel_color(9, light.rgb(255, 0, 0))
--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-
+// light.set_pixel_color(0, light.rgb(255, 0, 0))
+// light.set_pixel_color(1, light.rgb(0, 0, 255))
+// light.set_pixel_color(2, light.rgb(0, 255, 0))
+// light.set_pixel_color(3, light.rgb(255, 255, 0))
+// light.set_pixel_color(4, light.rgb(255, 0, 255))
+// light.set_pixel_color(5, light.rgb(0, 255, 255))
+// ^^^^^^^^^ Code for first part
+light.setPixelColor(0, light.rgb(255, 100, 0))
+light.setPixelColor(1, light.rgb(255, 255, 0))
+light.setPixelColor(2, light.rgb(0, 255, 0))
+light.setPixelColor(3, light.rgb(0, 255, 100))
+light.setPixelColor(4, light.rgb(0, 100, 125))
+light.setPixelColor(5, light.rgb(0, 75, 255))
+light.setPixelColor(6, light.rgb(0, 0, 255))
+light.setPixelColor(7, light.rgb(100, 100, 200))
+light.setPixelColor(8, light.rgb(255, 0, 105))
+light.setPixelColor(9, light.rgb(255, 0, 0))
