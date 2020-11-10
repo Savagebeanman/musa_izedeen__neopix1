77b0996c67954c4937ca1722b1a42dbe6d3ed9b3
Savagebeanman <72805052+Savagebeanman@users.noreply.github.com>
2020-11-10 11:19:26 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 light.set_brightness(25)
+#step 10 vvvvvvvvvv
 #light.set_pixel_color(0, light.rgb(255, 0, 0))
 #light.set_pixel_color(1, light.rgb(0, 0, 255))
 #light.set_pixel_color(2, light.rgb(0, 255, 0))
@@ -8,20 +9,6 @@ light.set_brightness(25)
 #^^^^^^^^^ Code for first part
 
 #challenge 1 vvvvvvvvvv
-light.set_pixel_color(0, light.rgb(255, 100, 0))
-light.set_pixel_color(1, light.rgb(255, 255, 0))
-light.set_pixel_color(2, light.rgb(0, 255, 0))
-light.set_pixel_color(3, light.rgb(0, 255, 100))
-light.set_pixel_color(4, light.rgb(0, 100, 125))
-light.set_pixel_color(5, light.rgb(0, 75, 255))
-light.set_pixel_color(6, light.rgb(0, 0, 255))
-light.set_pixel_color(7, light.rgb(100, 100, 200))
-light.set_pixel_color(8, light.rgb(255, 0, 105))
-light.set_pixel_color(9, light.rgb(255, 0, 0))
-
-#Challenge 2 vvvvvvvvv
-#light.set_brightness(100)
-#light.set_brightness(10)
 #light.set_pixel_color(0, light.rgb(255, 100, 0))
 #light.set_pixel_color(1, light.rgb(255, 255, 0))
 #light.set_pixel_color(2, light.rgb(0, 255, 0))
@@ -32,22 +19,77 @@ light.set_pixel_color(9, light.rgb(255, 0, 0))
 #light.set_pixel_color(7, light.rgb(100, 100, 200))
 #light.set_pixel_color(8, light.rgb(255, 0, 105))
 #light.set_pixel_color(9, light.rgb(255, 0, 0))
+
+#Challenge 2 vvvvvvvvv
+#light.set_brightness(100)
+#light.set_pixel_color(0, light.rgb(255, 100, 0))
+#light.set_pixel_color(1, light.rgb(255, 255, 0))
+#light.set_pixel_color(2, light.rgb(0, 255, 0))
+#light.set_pixel_color(3, light.rgb(0, 255, 100))
+#light.set_pixel_color(4, light.rgb(0, 100, 125))
+#light.set_pixel_color(5, light.rgb(0, 75, 255))
+#light.set_pixel_color(6, light.rgb(0, 0, 255))
+#light.set_pixel_color(7, light.rgb(100, 100, 200))
+#light.set_pixel_color(8, light.rgb(255, 0, 105))
+#light.set_pixel_color(9, light.rgb(255, 0, 0))
+#light.set_brightness(10)
   
-for i in range(10000):
-    pass
-    #Activity 2 blinking lights vvvvvvv
-    light.set_pixel_color(0, light.rgb(255, 100, 0))
-    light.set_pixel_color(1, light.rgb(255, 255, 0))
-    light.set_pixel_color(2, light.rgb(0, 255, 0))
-    light.set_pixel_color(3, light.rgb(0, 255, 100))
-    light.set_pixel_color(4, light.rgb(0, 100, 125))
-    light.set_pixel_color(5, light.rgb(0, 75, 255))
-    light.set_pixel_color(6, light.rgb(0, 0, 255))
-    light.set_pixel_color(7, light.rgb(100, 100, 200))
-    light.set_pixel_color(8, light.rgb(255, 0, 105))
-    light.set_pixel_color(9, light.rgb(255, 0, 0))
-    pause(3000)
+#for i in range(10):
+    #pass
+    #Challenge 3 blinking lights 10 times vvvvvvv
+    #light.set_pixel_color(0, light.rgb(255, 100, 0))
+    #light.set_pixel_color(1, light.rgb(255, 255, 0))
+    #light.set_pixel_color(2, light.rgb(0, 255, 0))
+    #light.set_pixel_color(3, light.rgb(0, 255, 100))
+    #light.set_pixel_color(4, light.rgb(0, 100, 125))
+    #light.set_pixel_color(5, light.rgb(0, 75, 255))
+    #light.set_pixel_color(6, light.rgb(0, 0, 255))
+    #light.set_pixel_color(7, light.rgb(100, 100, 200))
+    #light.set_pixel_color(8, light.rgb(255, 0, 105))
+    #light.set_pixel_color(9, light.rgb(255, 0, 0))
+    #pause(3000)
+    #light.clear()
+    #pause(2000)
+
+#while True:
+    #Challenge 3 blinking lights forever vvvvvvv
+    #light.set_pixel_color(0, light.rgb(255, 100, 0))
+    #light.set_pixel_color(1, light.rgb(255, 255, 0))
+    #light.set_pixel_color(2, light.rgb(0, 255, 0))
+    #light.set_pixel_color(3, light.rgb(0, 255, 100))
+    #light.set_pixel_color(4, light.rgb(0, 100, 125))
+    #light.set_pixel_color(5, light.rgb(0, 75, 255))
+    #light.set_pixel_color(6, light.rgb(0, 0, 255))
+    #light.set_pixel_color(7, light.rgb(100, 100, 200))
+    #light.set_pixel_color(8, light.rgb(255, 0, 105))
+    #light.set_pixel_color(9, light.rgb(255, 0, 0))
+    #pause(3000)
+    #light.clear()
+    #pause(2000)
+
+while True:
+    #Challenge 3 blinking lights forever vvvvvvv
+    light.set_pixel_color(0, light.rgb(255, 0, 0))
+    pause(300)
+    light.set_pixel_color(1, light.rgb(255, 100, 0))
+    pause(300)
+    light.set_pixel_color(2, light.rgb(255, 255, 0))
+    pause(300)
+    light.set_pixel_color(3, light.rgb(0, 255, 0))
+    pause(300)
+    light.set_pixel_color(4, light.rgb(0, 255, 100))
+    pause(300)
+    light.set_pixel_color(5, light.rgb(0, 100, 125))
+    pause(300)
+    light.set_pixel_color(6, light.rgb(0, 75, 255))
+    pause(300)
+    light.set_pixel_color(7, light.rgb(0, 0, 255))
+    pause(300)
+    light.set_pixel_color(8, light.rgb(100, 100, 200))
+    pause(300)
+    light.set_pixel_color(9, light.rgb(255, 0, 105))
+    pause(300)
     light.clear()
-    pause(2000)
+
     
 
